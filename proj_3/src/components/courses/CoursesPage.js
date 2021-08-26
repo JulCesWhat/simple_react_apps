4a963cc911c066030f801ee6e1b15b1fa04b947f
JulCesWhat <julceswhat@gmail.com>
2021-08-25 20:38:08 -0400

    Adding mock api and integration functions

--- proj_3/src/components/courses/CoursesPage.js
+++ proj_3/src/components/courses/CoursesPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { createCourse } from '../../redux/actions/courseActions';
+import * as courseActions from '../../redux/actions/courseActions';
 import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
 
 class CoursesPage extends React.Component {
     state = {
@@ -20,7 +21,7 @@ class CoursesPage extends React.Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
-        this.props.createCourse(this.state.course);
+        this.props.actions.createCourse(this.state.course);
     }
 
     render() {
@@ -42,7 +43,7 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
     courses: PropTypes.array.isRequired,
-    createCourse: PropTypes.func.isRequired
+    actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state) {
@@ -53,7 +54,7 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
     return {
-        createCourse: (course) => dispatch(createCourse(course))
+        actions: bindActionCreators(courseActions, dispatch)
     }
 }
 
