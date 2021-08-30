b5733ea9aedcfae6d5c8c2bde396a73118405923
JulCesWhat <julceswhat@gmail.com>
2021-08-29 22:51:22 -0400

    Adding reducer, store, thunk tests

--- proj_3/src/redux/actions/courseActions.test.js
+++ proj_3/src/redux/actions/courseActions.test.js
@@ -1,9 +1,37 @@
 import * as courseActions from './courseActions';
 import * as types from './actionTypes';
 import { courses } from '../../../tools/mockData';
-// import { describe } from 'yargs';
-// import { it } from 'jest-circus';
-// import { expect } from '@jest/globals';
+import thunk from 'redux-thunk';
+import fetchMock from 'fetch-mock';
+import configureMockStore from 'redux-mock-store';
+
+
+// Test an async action
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
+describe('Async actions', () => {
+    afterEach(() => {
+        fetchMock.restore();
+    });
+    describe('Load Courses Thunk', () => {
+        it('Should create BEGIN_API_CALL and LOAD_COURSES_SUCCESS when loading courses', () => {
+            fetchMock.mock('*', {
+                body: courses,
+                headers: { 'content-type': 'application/json' }
+            });
+            const expectedActions = [
+                { type: types.BEGIN_API_CALL },
+                { type: types.LOAD_COURSES_SUCCESS, courses }
+            ];
+            const store = mockStore({ courses: [] });
+            return store.dispatch(courseActions.loadCourses())
+                .then(() => {
+                    expect(store.getActions()).toEqual(expectedActions);
+                });
+        });
+    });
+});
 
 describe('createCourseSuccess', () => {
     it('Should create a CREATE_COURSE_SUCCESS action', () => {
---
+++ proj_3/src/redux/store.test.js
@@ -0,0 +1,21 @@
+import { createStore } from 'redux';
+import rootReducer from './reducer';
+import initialState from './reducer/initialState';
+import * as courseActions from './actions/courseActions';
+
+
+it('Should handle create course', () => {
+    // arrange
+    const store = createStore(rootReducer, initialState);
+    const course = {
+        title: 'Clean Code'
+    };
+
+    // act
+    const action = courseActions.createCourseSuccess(course);
+    store.dispatch(action)
+
+    // assert
+    const createdCourse = store.getState().courses[0];
+    expect(createdCourse).toEqual(course);
+});
